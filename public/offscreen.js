107be6f6e8b9591af194113a8f73a07935cdfe3e
Matthew James <matthew.stephen.james@gmail.com>
2025-01-02 13:56:06 +0800

    working end to end

--- public/offscreen.js
+++ public/offscreen.js
@@ -3,28 +3,82 @@
 let mediaRecorder;
 let connection;
 let {createClient} = deepgram;
-
-navigator.mediaDevices.enumerateDevices().then(function (devices) {
-  console.log("devices", devices.map((d) => d.label));
-  console.log("devices", devices);
-});
+let chosenDevice;
+let allDevices;
+let isMicOn = false;
+function stopAll() {
+  try {
+    mediaRecorder.stop();
+    mediaRecorder.stream.getTracks().forEach((track) => {
+      track.stop();
+    });
+    connection.close();
+  } catch (e) {
+    console.error("Error stopping mic", e);
+  }
+}
 
 function startMic() {
+  if (chosenDevice) {
+    startDeepgram();
+    return;
+  }
+  navigator.mediaDevices.enumerateDevices().then(function (devices) {
+    console.log("devices", devices.map((d) => d.label));
+    console.log("devices", devices);
+    chosenDevice = devices[0];
+    allDevices = devices;
+    startDeepgram();
+  });
+}
+
+startMic();
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.type === "mic-permission-granted") {
+    startMic();
+  } else if (message.type === "set-device") {
+    chosenDevice = message.device;
+    stopAll();
+    startDeepgram();
+  } else if (message.type === "request-devices") {
+    chrome.runtime.sendMessage({
+      type: "devices",
+      devices: allDevices,
+      chosenDevice: chosenDevice,
+    });
+  } else if (message.type === "stop-mic") {
+    stopAll();
+  } else if (message.type === "start-mic") {
+    startDeepgram();
+  } else if (message.type === "request-mic-status") {
+    chrome.runtime.sendMessage({
+      type: "mic-status",
+      isMicOn,
+    });
+  }
+});
+
+function startUserMedia() {
+  console.log("startUserMedia", chosenDevice);
   navigator.webkitGetUserMedia({
     audio: true,
+    deviceId: chosenDevice.deviceId,
   }, function(stream) {
-    console.log("started");
+    console.log("started webkit audio");
     mediaRecorder = new MediaRecorder(stream);
-    console.log("mediaRecorder", mediaRecorder, stream);
+
+    // console.log(stream.)
+    console.log("mediaRecorder", mediaRecorder, stream, stream.getTracks());
 
     mediaRecorder.ondataavailable = function(event) {
-      console.log("ondataavailable", event, connection);
+      // console.log("ondataavailable", event, connection);
       if (
         event.data.size > 0 &&
         connection != null &&
         connection.getReadyState() == WebSocket.OPEN
       ) {
-        console.log("sending dataavailable", event.data.size);
+        // console.log("sending dataavailable", event.data.size);
         connection.send(event.data);
       } else {
         console.log(
@@ -35,10 +89,48 @@ function startMic() {
       }
     };
 
-    mediaRecorder.start(250);
+    mediaRecorder.onstop = function() {
+      console.log("mediaRecorder onstop");
+      isMicOn = false;
+      chrome.runtime.sendMessage({
+        type: "mic-turned-off",
+      });
+    };
+
+    mediaRecorder.onerror = function(event) {
+      console.log("mediaRecorder onerror", event);
+      isMicOn = false;
+      chrome.runtime.sendMessage({
+        type: "mic-turned-off",
+      });
+    };
+
+    mediaRecorder.onpause = function(event) {
+      console.log("mediaRecorder onpause", event);
+      isMicOn = false;
+      chrome.runtime.sendMessage({
+        type: "mic-turned-off",
+      });
+    };
+
+    mediaRecorder.onresume = function(event) {
+      console.log("mediaRecorder onresume", event);
+      isMicOn = true;
+      chrome.runtime.sendMessage({
+        type: "mic-turned-on",
+      });
+    };
+
+
+    mediaRecorder.start(50);
+
+    isMicOn = true;
+    chrome.runtime.sendMessage({
+      type: "mic-turned-on",
+    });
   }, function() {
     // Aw. No permission (or no microphone available).
-    console.log("no permission");
+    console.log("webkit audio permission denied");
 
     chrome.runtime.sendMessage({
       type: "mic-permission-denied",
@@ -47,102 +139,97 @@ function startMic() {
   });
 }
 
-startMic();
+function startDeepgram() {
+  const client = createClient("162c389cf3aac80cc803d8533c2df3e683d73c59");
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.type === "mic-permission-granted") {
-    startMic();
-  }
-});
+  let is_finals = [];
 
-
-const client = createClient("162c389cf3aac80cc803d8533c2df3e683d73c59");
-
-let is_finals = [];
-
-connection = client.listen.live({
-  model: "nova-2",
-  language: `en-US`,
-  // Apply smart formatting to the output
-  smart_format: true,
-  // To get UtteranceEnd, the following must be set:
-  interim_results: true,
-  utterance_end_ms: 1000,
-  vad_events: true,
-  // Time in milliseconds of silence to wait for before finalizing speech
-  endpointing: 300,
-});
-
-connection.on("open", function () {
-  console.log("Connection opened.");
-
-  connection.on("close", () => {
-    console.log("Connection closed.");
-    const utterance = is_finals.join(" ");
-    console.log(`Speech Final: ${utterance}`);
-    chrome.runtime.sendMessage({
-      type: "speech-final",
-      message: utterance,
-    });
-    is_finals = [];
-    mediaRecorder.stop();
-    mediaRecorder.stream.getTracks().forEach((track) => {
-      track.stop();
-    });
+  connection = client.listen.live({
+    model: "nova-2",
+    language: `en-US`,
+    // Apply smart formatting to the output
+    smart_format: true,
+    // To get UtteranceEnd, the following must be set:
+    interim_results: true,
+    utterance_end_ms: 1000,
+    vad_events: true,
+    // Time in milliseconds of silence to wait for before finalizing speech
+    endpointing: 300,
   });
 
-  connection.on("Metadata", (data) => {
-    console.log(`Deepgram Metadata: ${data}`);
-  });
+  connection.on("open", function () {
+    console.log("Connection opened.");
 
-  connection.on("Results", (data) => {
-    const sentence = data.channel.alternatives[0].transcript;
+    connection.on("close", () => {
+      console.log("Connection closed.");
+      const finals = is_finals.join(" ");
+      console.log(`Speech finals onclose: ${finals}`);
+      chrome.runtime.sendMessage({
+        type: "speech-final",
+        message: finals,
+      });
+      is_finals = [];
+      mediaRecorder.stop();
+      mediaRecorder.stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+    });
+
+    connection.on("Metadata", (data) => {
+      console.log(`Deepgram Metadata:`, data);
+    });
+
+    connection.on("Results", (data) => {
+      const sentence = data.channel.alternatives[0].transcript;
 
 
-    // Ignore empty transcripts
-    if (sentence.length == 0) {
-      return;
-    }
-    if (data.is_final) {
-      // We need to collect these and concatenate them together when we get a speech_final=true
-      // See docs: https://developers.deepgram.com/docs/understand-endpointing-interim-results
-      is_finals.push(sentence);
-
-      // Speech final means we have detected sufficent silence to consider this end of speech
-      // Speech final is the lowest latency result as it triggers as soon an the endpointing value has triggered
-      if (data.speech_final) {
-        const utterance = is_finals.join(" ");
-        console.log(`Speech Final: ${utterance}`);
-        chrome.runtime.sendMessage({
-          type: "speech-final",
-          message: utterance,
-        });
-        is_finals = [];
-      } else {
-        // These are useful if you need real time captioning and update what the Interim Results produced
-        console.log(`Is Final: ${sentence}`);
-        chrome.runtime.sendMessage({
-          type: "interim-results",
-          message: sentence,
-        });
+      // Ignore empty transcripts
+      if (sentence.length == 0) {
+        return;
       }
-    } else {
-      // These are useful if you need real time captioning of what is being spoken
-      console.log(`Interim Results: ${sentence}`);
-    }
-  });
+      if (data.is_final) {
+        // We need to collect these and concatenate them together when we get a speech_final=true
+        // See docs: https://developers.deepgram.com/docs/understand-endpointing-interim-results
+        is_finals.push(sentence);
 
-  connection.on("UtteranceEnd", (data) => {
-    const utterance = is_finals.join(" ");
-    console.log(`Deepgram UtteranceEnd: ${utterance}`);
-    is_finals = [];
-  });
+        // Speech final means we have detected sufficent silence to consider this end of speech
+        // Speech final is the lowest latency result as it triggers as soon an the endpointing value has triggered
+        if (data.speech_final) {
+          const finals = is_finals.join(" ");
+          console.log(`Speech Final onresults: ${finals}`);
+          chrome.runtime.sendMessage({
+            type: "speech-final",
+            message: finals,
+          });
+          is_finals = [];
+        } else {
+          // These are useful if you need real time captioning and update what the Interim Results produced
+          console.log(`Is interim onresults: ${sentence}`);
+          chrome.runtime.sendMessage({
+            type: "interim-results",
+            message: sentence,
+          });
+        }
+      } else {
+        // These are useful if you need real time captioning of what is being spoken
+        console.log(`Interim Results: ${sentence}`);
+      }
+    });
 
-  connection.on("SpeechStarted", (data) => {
-    console.log("Deepgram SpeechStarted");
-  });
+    connection.on("UtteranceEnd", (data) => {
+      const finals = is_finals.join(" ");
+      console.log(`Deepgram UtteranceEnd: ${finals}`);
+      is_finals = [];
+    });
 
-  connection.on("error", (err) => {
-    console.error(err);
+    connection.on("SpeechStarted", (data) => {
+      console.log("Deepgram SpeechStarted");
+    });
+
+    connection.on("error", (err) => {
+      console.error(err);
+    });
+
+    startUserMedia();
   });
-});
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { useEffect } from "react";
 
 declare global {
@@ -5,24 +7,91 @@ declare global {
 }
 
 function App() {
+  const [devices, setDevices] = useState<any>([]);
+  const [chosenDevice, setChosenDevice] = useState<any>(null);
+  const [isMicOn, setIsMicOn] = useState<boolean>(false);
+  const [speeches, setSpeeches] = useState<any>([]);
+
   useEffect(() => {
     chrome.runtime.onMessage.addListener(function (
-      request: any,
-      sender: any,
-      _: any
+      request: any
+      // sender: any,
+      // _: any
     ) {
-      console.log(
-        sender.tab
-          ? "from a content script:" + sender.tab.url
-          : "from the extension:",
-        JSON.stringify(request)
-      );
+      console.log("request", request);
+      if (request.type === "devices") {
+        console.log("devices", request.devices);
+        console.log("chosenDevice", request.chosenDevice);
+        setDevices(request.devices);
+        setChosenDevice(request.chosenDevice);
+      } else if (request.type === "mic-turned-on") {
+        setIsMicOn(true);
+      } else if (request.type === "mic-turned-off") {
+        setIsMicOn(false);
+      } else if (request.type === "speech-final") {
+        setSpeeches((speeches: any) => [...speeches, request.message]);
+      }
+    });
+
+    chrome.runtime.sendMessage({
+      type: "request-devices",
+    });
+
+    chrome.runtime.sendMessage({
+      type: "request-mic-status",
     });
   }, []);
+
+  const handleDeviceChange = (e: any) => {
+    const deviceId = e.target.value;
+    const device = devices.find((device: any) => device.deviceId === deviceId);
+    console.log("device", device);
+    setChosenDevice(device);
+    chrome.runtime.sendMessage({
+      type: "set-device",
+      device,
+    });
+  };
+
+  const handleMicToggle = () => {
+    if (isMicOn) {
+      chrome.runtime.sendMessage({
+        type: "stop-mic",
+      });
+    } else {
+      chrome.runtime.sendMessage({
+        type: "start-mic",
+      });
+    }
+  };
 
   return (
     <div className="w-[300px] h-[500px] ">
       <h1 className="text-3xl font-bold underline p-8">Hello world!</h1>
+      <div>
+        <h2>Devices</h2>
+        {/* <ul>
+          {devices.map((device: any) => (
+            <li key={device.deviceId}>{device.label}</li>
+          ))}
+        </ul> */}
+
+        <select value={chosenDevice?.deviceId} onChange={handleDeviceChange}>
+          {devices.map((device: any) => (
+            <option key={device.deviceId} value={device.deviceId}>
+              {device.label}
+            </option>
+          ))}
+        </select>
+
+        <button onClick={handleMicToggle}>
+          {isMicOn ? "Turn off mic" : "Turn on mic"}
+        </button>
+
+        {speeches.map((speech: any) => (
+          <p key={speech}>{speech}</p>
+        ))}
+      </div>
     </div>
   );
 }
